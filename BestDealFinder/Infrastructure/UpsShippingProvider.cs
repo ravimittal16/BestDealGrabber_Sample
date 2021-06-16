9bfd2ca8b62c9a188bdf60aed3a8a9b880c66025
Ravi Mittal <ravimittal16@gmail.com>
2021-06-16 07:58:58 +0530

    Another way Json

--- BestDealFinder/Infrastructure/UpsShippingProvider.cs
+++ BestDealFinder/Infrastructure/UpsShippingProvider.cs
@@ -17,12 +17,13 @@ namespace BestDealFinder.Infrastructure
 
         public override string GetApiAcceptedDataFormat()
         {
-            //we can update the request data here, before sending to api...
-            dynamic jsonObject = new JObject();
-            jsonObject.consignee = JObject.FromObject(_requestModel?.ContactAddress);
-            jsonObject.consignor = JObject.FromObject(_requestModel?.WarehouseAddress);
-            jsonObject.cartons = new JArray(_requestModel?.Dimensions);
-            return jsonObject.ToString();
+            //another way of serilizing to desired object
+            dynamic jsonObject = new
+            {
+                consignee = _requestModel?.ContactAddress, consignor = _requestModel?.WarehouseAddress,
+                cartons = _requestModel?.Dimensions
+            };
+            return JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
         }
         public override IShippingProviderApiDetails ShippingProviderApiDetails =>
             new ShippingProviderApiDetails(ResponseTypes.Json, "https://60c629c319aa1e001769eec7.mockapi.io/api/upsPrice", new ApiCredentials("ups___consumer__key", "ups___consumer__secert"));
